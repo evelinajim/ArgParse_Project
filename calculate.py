d7eb81452bdb4e8287cf9a0b437f2ef7a57a87a3
evelinajim <36286948+evelinajim@users.noreply.github.com>
2019-06-10 21:57:18 -0400

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""
+import argparse
+parser = argparse.ArgumentParser()
+#parser.add_argument("-a","--add",nargs="+",help="adds numbers together")
+parser.add_argument("square", help="display a square of a given number",
+                    type=int)
+args = parser.parse_args()
+print(args.square**2)
+
+"""
+"""
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2],
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbosity == 2:
+    print("the square of {} equals {}".format(args.square, answer))
+elif args.verbosity == 1:
+    print("{}^2 == {}".format(args.square, answer))
+else:
+    print(answer)
+"""
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("x", type=int, help="first integer")
+parser.add_argument("y", type=int, help="second integer")
+parser.add_argument("-a","--add", help="adds two given numbers together")
+parser.add_argument("-m","--multiply", help="multiplies two given numbers together")
+parser.add_argument("-v", "--verbosity", action="count", default=0,
+                    help="increase output verbosity")
+args = parser.parse_args()
+summation = args.x+args.y
+product = args.x*args.y
+
+if args.add:
+    if args.verbosity >= 2:
+        print("{} plus {} equals {}".format(args.x, args.y, summation))
+    elif args.verbosity >= 1:
+        print("{}+{} == {}".format(args.x, args.y, summation))
+    else:
+        print(summation)
+elif args.multiply:
+    if args.verbosity >= 2:
+        print("{} multiplied by {} equals {}".format(args.x, args.y, product))
+    elif args.verbosity >= 1:
+        print("{}*{} == {}".format(args.x, args.y, product))
+    else:
+        print(product)
+else:
+    if args.verbosity >= 2:
+        print("{} plus {} equals {}".format(args.x, args.y, summation))
+    elif args.verbosity >= 1:
+        print("{}+{} == {}".format(args.x, args.y, summation))
+    else:
+        print(summation)
+
+"""
+#untouched verbosity code
+if args.verbosity >= 2:
+    print("{} plus {} equals {}".format(args.x, args.y, summation))
+elif args.verbosity >= 1:
+    print("{}+{} == {}".format(args.x, args.y, summation))
+else:
+    print(summation)
+"""
